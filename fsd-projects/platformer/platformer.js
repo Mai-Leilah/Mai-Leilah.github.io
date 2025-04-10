192453584d091e6c93f9fabaf51b8702e4a527fd
Mai-Leilah <leimir937@gmail.com>
2025-04-10 23:04:04 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -1,5 +1,5 @@
 $(function () {
-  // initialize canvas and context when able to
+   //initialize canvas and context when able to
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
   window.addEventListener("load", loadJson);
@@ -27,21 +27,31 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-createPlatform(500,150,200,20, "green");
-createPlatform()
+createPlatform(120,600,200,20, "blue");
+createPlatform(500,500,200,20, "blue");
+createPlatform(350,300,200,20, "blue");
+createPlatform(800,420,200,20, "blue");
+createPlatform(1100,250,200,20, "blue");
+createPlatform(720,160,200,20, "blue");
+
+
 
 
     // TODO 3 - Create Collectables
-
+createCollectable("database",880,380,0.9,0.8);
+createCollectable("database",450,270,0.9,0.8);
+createCollectable("database",800,130,0.9,0.8);
 
 
     
     // TODO 4 - Create Cannons
-
+createCannon("top",1220,200);
+createCannon("right",400,100);
+createCannon("top",700,200);
 
     
     
