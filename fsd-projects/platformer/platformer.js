a1faf4c3277822a7114cadb102f483dcafe7174a
Mai-Leilah <leimir937@gmail.com>
2025-05-06 23:52:54 +0000

    platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -1,5 +1,5 @@
 $(function () {
-   //initialize canvas and context when able to
+  //initialize canvas and context when able to
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
   window.addEventListener("load", loadJson);
@@ -27,38 +27,27 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    toggleGrid();
-
 
     // TODO 2 - Create Platforms
-createPlatform(150,650,150,20, "hotpink");
-createPlatform(470,570,150,20, "hotpink");
-createPlatform(380,200,150,20, "hotpink");
-createPlatform(760,420,150,20, "hotpink");
-createPlatform(1100,250,150,20, "hotpink");
-createPlatform(720,160,150,20, "hotpink");
-createPlatform(300,300, 150,20, "hotpink");
-createPlatform(110,460,180,20,"hotpink");
-createPlatform();
-
-
-
+    createPlatform(150, 650, 150, 20, "hotpink");
+    createPlatform(470, 570, 150, 20, "hotpink");
+    createPlatform(380, 200, 150, 20, "hotpink");
+    createPlatform(760, 420, 150, 20, "hotpink");
+    createPlatform(1100, 250, 150, 20, "hotpink");
+    createPlatform(720, 160, 150, 20, "hotpink");
+    createPlatform(300, 300, 150, 20, "hotpink");
+    createPlatform(110, 460, 180, 20, "hotpink");
 
     // TODO 3 - Create Collectables
-createCollectable("database",480,380,0.9,0.8);
-createCollectable("database",1130,220,0.9,0.8);
-createCollectable("database",800,130,0.9,0.8);
+    createCollectable("database", 480, 380, 0.9, 0.8);
+    createCollectable("database", 1130, 220, 0.9, 0.8);
+    createCollectable("database", 800, 130, 0.9, 0.8);
 
-
-    
     // TODO 4 - Create Cannons
-createCannon("top",1220,200);
-createCannon("right",300,2000);
-createCannon("top",605,10 00);
+    createCannon("top", 1220, 200);
+    createCannon("right", 300, 2000);
+    createCannon("top", 605, 1000);
 
-
-    
-    
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
